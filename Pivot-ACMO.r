562aaecac3f5ab8e4c020d4dd27e84cd735c3bea
Cheryl Porter <cporter@ufl.edu>
2016-03-31 08:22:37 -0400

    Added some comments

--- Pivot-ACMO.r
+++ Pivot-ACMO.r
@@ -15,35 +15,44 @@
 
 ### AgMIP Regional Integrated Assessments: Pivot-ACMO ##
 
-# This routine takes crop model outputs (ACMO format) and pivots the specified variable to create a table with farms as rows and 30 years of output as columns.
+# This routine takes crop model outputs (ACMO format) and creates single variable csv files.
+#  - Each output file represents a single variable and a single ACMO file.
+#  - The table contains 30 columns representing the years 1980-2009 and
+#    N rows, one row per farm.
+#  - File names are "Variable name"-"ACMO name".csv
 # Created: March 30, 2016
 # Last edited: 
 # Author: Cheryl Porter, cporter@ufl.edu
-# Co-authors: Sonali McDermid
-# Routines are not gauranteed, and any help questions should be directed to the authors
 
-# It is suggested that the User read carefully through this routine, and specifically note those lines that contain, or are preceeded by the word "CHANGE" in uppercase. These are areas that the User will need to change the inputs. The program is constructed to minimize the number of changes that the User makes, and can be modified by the User for her/his needs. These "CHANGE" statements can usually be found at the top of each section. 
-
-#Prior to using this routine, the user must Install Package "reshape2" MUST BE INSTALLED FIRST!
+#---- REQUIRED LIBRARIES -------------------------------------------#
+#PRIOR TO USING THIS ROUTINE, THE R PACKAGE "reshape2" MUST BE INSTALLED!
 
 library(reshape2)
-#----------------------DEFINE VARIABLES OF INTEREST HERE-------------------------#
+
+#---- DEFINE VARIABLES OF INTEREST HERE ----------------------------#
+# The user can cahnge the variable(s) of interest by listing them here. 
+#  - Any subset of ACMO variables may be listed. 
+#  - A separate file will be created for each variable-ACMO file combination.
 
 varnames <- c('HWAH_S','CWAH_S')
 
-#----------------------Start Routine-------------------------#
+#---- Start Routine ------------------------------------------------#
 
 # Definitions
 years <-c(1980:2009) # Assume 30 years (1980-2009) for all simulations
 
-# List all ACMO files
+# List all files in the current directory
 files <- list.files(path=".", full.names=FALSE, recursive=FALSE)
 
+# loop thru all files in the current directory
 for(f in 1:length(files)) {
   acmo_name <- files[f]
+
+  # look for filenames that start with "ACMO-"
   if (strtrim(acmo_name,5) == "ACMO-") {
     
-    farmdata <- read.csv(file=acmo_name,skip = 2, head=TRUE,sep=",") # This is for ACMO files that have a header above column names
+    # This is for ACMO files that have a header above column names
+    farmdata <- read.csv(file=acmo_name,skip = 2, head=TRUE,sep=",")
     EXNAME_long <- farmdata$EXNAME
     number_of_farms <- length(EXNAME_long)
     exname <- vector(mode="character", length = number_of_farms)
@@ -53,12 +62,12 @@ for(f in 1:length(files)) {
       exname[i] <- unlist(strsplit(as.character(EXNAME_long[i]),split='__',fixed=TRUE))[1]
     }
 
-    #write a new pivot table for each combination of variable of interest plus acmo file.
+    #write a new pivot table for each combination of variable of interest + acmo file.
     for (v in 1:length(varnames)) {
       #create a dataframe with only exname, years (1980-2009) and the variable of interest (one at a time)
       farmdata2 <- data.frame(exname, years, farmdata[varnames[v]])
   
-      #pivot table - requires library dplyr
+      #pivot table - requires library reshape2
       #takes farmdata2, farmname is rows, years is columns, values are the varible of interest
       farmdata3 <- dcast(farmdata2, exname ~ years, value.var = varnames[v])
       
